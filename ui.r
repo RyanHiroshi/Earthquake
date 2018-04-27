d7ded5b88278ca8e1bd611178a50dfcfddbf119f
RyanHiroshi <35421157+RyanHiroshi@users.noreply.github.com>
2018-04-27 07:34:42 +0700

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,10 +1,11 @@
 library(shiny)
 library(shinydashboard)
 library(leaflet)
-
+## leafletOutput is used at the ui side to display the rendered map.
+EQ=read.csv("query (3).csv")
 
 shinyUI(fluidPage(
-  dashboardHeader(title = "Eartquake Position | Hover for Magnitude"),
   sliderInput("count","Amount of Earthquakes",10,10000,20),
   leafletOutput("map")
 ))
+
